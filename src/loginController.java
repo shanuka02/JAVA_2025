593ce6175d9181ab88352a3535d5d4cdabdbdf09
Poojani-janitha <jayarathnahpj23@gmail.com>
2025-04-11 14:08:43 +0530

    add controller fies

--- src/loginController.java
+++ src/loginController.java
@@ -1,5 +1,8 @@
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
@@ -7,8 +10,11 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
 
+import javax.imageio.IIOException;
 import javax.swing.*;
+import java.io.IOException;
 import java.sql.*;
 
 public class loginController{
@@ -58,10 +64,19 @@ public class loginController{
 
             ResultSet  rs = pstmt.executeQuery();
             if (rs.next()) {
-                JOptionPane.showMessageDialog(null, "Login successful!");
+                // if roll is admin   direct to admininterface.java
+                // if roll is undergraduate   direct to undergraduateinterface.java
+                //JOptionPane.showMessageDialog(null, "Login successful!");
 
-// Error popup
-                // You can now redirect or load the next scene
+                String role =  rs.getString("role");
+                if("admin".equalsIgnoreCase(role)){
+                    loadInterface("AdminInterface.fxml");
+                }else if("undergraduate".equalsIgnoreCase(role)){
+                    loadInterface("UndergraduateInterface.fxml");
+                }else if("lecture".equalsIgnoreCase(role)){
+                    loadInterface("lectureInterface.fxml");
+                }
+
             } else {
                 JOptionPane.showMessageDialog(null, "Invalid username or password.", "Login Failed", JOptionPane.ERROR_MESSAGE);
 
@@ -72,5 +87,21 @@ public class loginController{
         }
 
 
+    }
+
+
+    private void loadInterface(String fxmlFile) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlFile));
+            Parent root = loader.load();
+
+            // Assuming you have a reference to the current stage
+            Stage stage = (Stage) mainBorder.getScene().getWindow();
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+            JOptionPane.showMessageDialog(null, "Failed to load interface.", "Error", JOptionPane.ERROR_MESSAGE);
+        }
     }
 }
