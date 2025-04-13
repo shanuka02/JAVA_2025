680db6a0b20073a38a683186e61ae689415b646f
Poojani-janitha <jayarathnahpj23@gmail.com>
2025-04-13 21:57:25 +0530

    change files

--- src/SearchUserController.java
+++ src/SearchUserController.java
@@ -1,2 +1,12 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class SearchUserController {
+    @FXML
+    private Button Button1;
+
+    @FXML
+    private Button Button2;
+
+
 }
