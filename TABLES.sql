a1eef9211fbe02dd2beddbc5bfa33395588f61d2
Vihaga256 <vihagayasithawijewardhana29@gmail.com>
2025-04-19 03:28:08 +0530

    Error correction

--- TABLES.sql
+++ TABLES.sql
@@ -1,32 +1,48 @@
 CREATE TABLE attendance (
-    Att_id INT PRIMARY KEY AUTO_INCREMENT,
-    Att_stu_id CHAR(8) NOT NULL,
-    Att_cou_id CHAR(8) NOT NULL,
-    Pre_date DATE,
-    Pre_time TIME,
-    Lec_hours INT,
-    Lec_type CHAR(2),
-    Statu CHAR(2) NOT NULL,
-    Att_medi_id CHAR(8),
-    FOREIGN KEY (Att_stu_id) REFERENCES student(student_id) ON UPDATE CASCADE,
-    FOREIGN KEY (Att_cou_id) REFERENCES course(Co_id) ON UPDATE CASCADE,
-    FOREIGN KEY (Att_medi_id) REFERENCES medical(medi_id) ON UPDATE CASCADE
+                            Att_id INT PRIMARY KEY AUTO_INCREMENT,
+                            Att_stu_id CHAR(8) NOT NULL,
+                            Att_cou_id CHAR(8) NOT NULL,
+                            Pre_date DATE,
+                            Pre_time TIME,
+                            Lec_hours INT,
+                            Lec_type ENUM('Lecture', 'Practical'),
+                            Status_ ENUM('Present', 'Absent' , 'Medical') DEFAULT 'Absent',
+                            Att_medi_id INT,
+                            FOREIGN KEY (Att_stu_id) REFERENCES student(student_id) ON UPDATE CASCADE,
+                            FOREIGN KEY (Att_cou_id) REFERENCES course(Co_id) ON UPDATE CASCADE,
+                            FOREIGN KEY (Att_medi_id) REFERENCES medical(Medi_id) ON UPDATE CASCADE
 );
 /*Only first run*/
-INSERT INTO attendance (Att_id, Att_stu_id, Att_cou_id, Pre_date, Pre_time, Lec_hours, Lec_type, Statu) VALUES
-(1,'TG1301','ICT1233','2025-04-05','14:30:00',2,'t','p');
+INSERT INTO attendance (Att_id, Att_stu_id, Att_cou_id, Pre_date, Pre_time, Lec_hours, Lec_type, Status_) VALUES
+    (1,'TG1301','ICT1233','2025-04-05','14:30:00',2,'Lecture','Present');
 
-/*data*/ 
-INSERT INTO attendance (Att_stu_id, Att_cou_id, Pre_date, Pre_time, Lec_hours, Lec_type, Statu) VALUES
-('TG1301','ICT1233','2025-04-05','14:30:00',2,'t','p'),
-('TG1303','ICT1233','2025-04-05','14:30:00',2,'t','p'),
-('TG1304','ICT1233','2025-04-05','14:30:00',2,'t','p'),
-('TG1305','ICT1233','2025-04-05','14:30:00',2,'t','p'),
-('TG1306','ICT1233','2025-04-05','14:30:00',2,'t','p'),
-('TG1307','ICT1233','2025-04-05','14:30:00',2,'t','a');
+/*data*/
+INSERT INTO attendance (Att_stu_id, Att_cou_id, Pre_date, Pre_time, Lec_hours, Lec_type, Status_) VALUES
+    ('TG1301','ICT1233','2025-04-05','14:30:00',2,'Lecture','Present'),
+    ('TG1303','ICT1233','2025-04-05','14:30:00',2,'Lecture','Present'),
+    ('TG1304','ICT1233','2025-04-05','14:30:00',2,'Lecture','Present'),
+    ('TG1305','ICT1233','2025-04-05','14:30:00',2,'Lecture','Absent'),
+    ('TG1306','ICT1233','2025-04-05','14:30:00',2,'Lecture','Present'),
+    ('TG1307','ICT1233','2025-04-05','14:30:00',2,'Lecture','Present');
 
 ('TG1303','ICT1233',1,1,'t','T01'),
 ('TG1306','ICT1233',1,1,'t','T01'),
 ('TG1309','ICT1233',1,1,'t','T01'),
 ('TG1311','ICT1233',1,1,'t','T01'),
 ('TG1312','ICT1233',1,1,'t','T01');
+
+
+CREATE TABLE medical(
+                        Medi_id INT PRIMARY KEY AUTO_INCREMENT,
+                        Me_stu_id CHAR(8) NOT NULL,
+                        Me_cou_id CHAR(8) NOT NULL,
+                        Reason VARCHAR(250) NOT NULL,
+                        Request_date DATE,
+                        Status_ ENUM('rejected', 'pending', 'approved') DEFAULT 'pending',
+                        Submitted_date DATE,
+                        FOREIGN KEY (Me_stu_id) REFERENCES student(Student_id) ON UPDATE CASCADE,
+                        FOREIGN KEY (Me_cou_id) REFERENCES course(Co_id) ON UPDATE CASCADE
+);
+
+INSERT INTO medical()VALUES
+    ();
--- src/TechnicalMain.java
+++ src/TechnicalMain.java
@@ -2,12 +2,14 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class TechnicalMain {
+    private static Stage stage = new Stage();
+
     public void Go(ActionEvent event) throws IOException {
-//        System.out.println("Technical Officer");
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/resources/fxml/TechnicalOfficer.fxml"));
         try{
             Parent root = loader.load();
@@ -16,5 +18,32 @@ public class TechnicalMain {
         }catch (Exception e){
             System.out.println("Error_code: x0000e1.2 " + e.getMessage());
         }
+    }
+
+    public void attendanceBtn(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/resources/fxml/Selection1.fxml"));
+        try{
+            Parent root = loader.load();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.setTitle("Attendance");
+            stage.setResizable(false);
+            stage.setX(500);
+            stage.setY(300);
+            stage.show();
+
+        }catch (Exception e){
+            System.out.println("Error_code: x0000e1.3 " + e.getMessage());
+        }
+    }
+
+    public void attendanceAdd(ActionEvent event) throws IOException {
+        stage.close();
+        System.out.println("Attendance Added");
+    }
+
+    public void attendanceView(ActionEvent event) throws IOException {
+        stage.close();
+        System.out.println("Attendance Viewed");
     }
 }
