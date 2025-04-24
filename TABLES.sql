4ddc96667a7884b88649675f6d6634f1cb50c2a9
Vihaga256 <vihagayasithawijewardhana29@gmail.com>
2025-04-24 16:54:47 +0530

    Add class and fxml

--- TABLES.sql
+++ TABLES.sql
@@ -27,9 +27,10 @@ CREATE TABLE medical(
                         Medi_id INT PRIMARY KEY AUTO_INCREMENT,
                         Me_stu_id CHAR(8) NOT NULL,
                         Me_cou_id CHAR(8) NOT NULL,
-                        Lec_type ENUM('theory', 'practical'),
+                        Lec_type ENUM('Theory', 'Practical'),
                         Reason VARCHAR(250) NOT NULL,
                         Request_date DATE,
+                        Status_ ENUM('pending','approved','rejected'),
                         Submitted_date DATE,
                         FOREIGN KEY (Me_stu_id) REFERENCES userAccount(user_id) ON UPDATE CASCADE,
                         FOREIGN KEY (Me_cou_id) REFERENCES courseUnit(courseId) ON UPDATE CASCADE
@@ -37,12 +38,12 @@ CREATE TABLE medical(
 
 INSERT INTO medical (Medi_id, Me_stu_id, Me_cou_id, Lec_type, Reason, Request_date, Status_, Submitted_date)
 VALUES
-    (1, 'TG1301', 'ICT1233','theory', 'Fever and flu for two days', '2025-04-20',  '2025-04-21');
+    (1, 'TG1301', 'ICT1233','Theory', 'Fever and flu for two days', '2025-04-20', 'pending', '2025-04-21');
 
 INSERT INTO medical (Me_stu_id, Me_cou_id, Lec_type, Reason, Request_date, Status_, Submitted_date)
 VALUES
-    ('TG1301', 'ICT2133', 'theory', 'Fever and flu for two days', '2025-04-20',  '2025-04-21'),
-    ('TG1305', 'ICT1234', 'theory','Surgery recovery', '2025-04-18', '2025-04-20'),
-    ('TG1304', 'ICT1240', 'theory', 'Family emergency during lectures', '2025-04-17',  '2025-04-20'),
-    ('TG1301', 'ICT1239', 'theory','Accident – unable to attend', '2025-04-16',  '2025-04-18'),
-    ('TG1302', 'ICT1236', 'theory','Hospitalized for dengue', '2025-04-15', '2025-04-18');
+    ('TG1304', 'ICT2122', 'Theory', 'Fever and flu for two days', '2025-04-05',  'approved','2025-04-21'),
+    ('TG1305', 'ICT1234', 'Theory','Surgery recovery', '2025-04-18','pending', '2025-04-20'),
+    ('TG1304', 'ICT1240', 'Theory', 'Family emergency during lectures', '2025-04-17',  'pending','2025-04-20'),
+    ('TG1302', 'ICT2113', 'Theory','Accident – unable to attend', '2025-04-25',  'approved','2025-04-18'),
+    ('TG1301', 'ICT1236', 'Theory','Hospitalized for dengue', '2025-04-15', 'rejected','2025-04-18');
--- src/AttendanceMedical.java
+++ src/AttendanceMedical.java
@@ -1,28 +1,49 @@
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 import static java.lang.Integer.parseInt;
 
 public class AttendanceMedical {
     public void setAttendance(){
-        String quary = "SELECT * FROM Medical WHERE Status_ = 'approved'";
-        int name;
+        String quary1 = "SELECT Medi_id, Me_stu_id, Me_cou_id, Lec_type, Request_date FROM Medical WHERE Status_ = 'approved'";
+        String quary2 = "SELECT Att_id, Att_stu_id, Att_cou_id, Lec_type, Pre_date FROM attendance WHERE status_ = 'Absent' AND Att_medi_id IS NULL";
+
         Connection connection = DBConnection.getConnection();
-        Statement statement = null;
-        ResultSet results = null;
         try {
-            assert connection != null;
-            statement = connection.createStatement();
-            results = statement.executeQuery(quary);
-            while (results.next()) {
-                System.out.println( results.getString(1) + " " +results.getString(2) + " " + results.getString(3) + " " + results.getString(4));
-                name = parseInt(results.getString(1));
+            assert connection != null;  //medical table
+            Statement statement1 = connection.createStatement();
+            ResultSet results1 = statement1.executeQuery(quary1);
+
+            Statement statement2 = connection.createStatement();
+            ResultSet results2 = statement2.executeQuery(quary2);
+            while (results1.next()) {
+                while (results2.next()){
+                    if (results2.getString(2).equals(results1.getString(2)) && results2.getString(3).equals(results1.getString(3)) && results2.getString(4).equals(results1.getString(4)) && results2.getString(5).equals(results1.getString(5))){
+                        saveAttendance(parseInt(results1.getString(1)), parseInt(results2.getString(1)), connection);
+                        break;
+                    }
+                }
             }
+            connection.close();
         } catch (SQLException e) {
 
+        }
+    }
+
+    private void saveAttendance(int medical_id, int attendance_id, Connection connection) throws SQLException {
+        String quary = "UPDATE attendance SET Status_ = ?, Att_medi_id = ? WHERE Att_id = ?";
+
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(quary);
+            preparedStatement.setString(1,"Medical");
+            preparedStatement.setInt(2, medical_id);
+            preparedStatement.setInt(3, attendance_id);
+            int result= preparedStatement.executeUpdate();
+            if (result > 0) {
+                System.out.println("Attendance updated");
+            }
+        }catch (SQLException e){
+            System.out.println("Error updating attendance " + e.getMessage());
         }
     }
 
