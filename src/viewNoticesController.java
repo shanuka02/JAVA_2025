43087a3dea521a813959165a8a6eb2ccca0fdd38
sahancliyanage <chamikaliyanage2002322@gmail.com>
2025-04-25 12:35:00 +0530

    implement attendence details

--- src/viewNoticesController.java
+++ src/viewNoticesController.java
@@ -3,16 +3,5 @@ import javafx.scene.control.TableColumn;
 
 public class viewNoticesController {
 
-    @FXML
-    private TableColumn<?, ?> noticeData;
-
-    @FXML
-    private TableColumn<?, ?> noticeNumber;
-
-    @FXML
-    private TableColumn<?, ?> noticeTitle;
-
-    @FXML
-    private TableColumn<?, ?> previewButton;
 
 }
