c0207dfc7ea30ef39146d4a7fe3a6febb58dbe8b
shanuka02 <shanukagamlath@gmail.com>
2025-04-25 13:08:42 +0530

    view and images

--- src/ProgramView.java
+++ src/ProgramView.java
@@ -4,6 +4,10 @@ import javafx.scene.control.ProgressBar;
 import javafx.scene.text.Text;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class ProgramView {
     public void backtopage() throws IOException {
@@ -17,9 +21,23 @@ public class ProgramView {
     private ProgressBar PresentageBar;
 
 
+public void Viewpresentageict01(){
+
+    String quary = "SELECT * FROM attendance;";
+    try {
+        Connection connection  = DBConnection.getConnection();
+        assert connection != null;
+        Statement statement = connection.createStatement();
+        ResultSet rs = statement.executeQuery(quary);
+
+        while (rs.next()) {
+            String courseCode = rs.getString("courseCode");
 
 
-
-
-
+        }
+    }catch (
+            SQLException e) {
+        e.printStackTrace();
+    }
+}
 }
