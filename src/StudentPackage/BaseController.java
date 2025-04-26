23d111b3f65bb3704082c516db6393b68d1b14d7
shanuka02 <shanukagamlath@gmail.com>
2025-04-26 18:55:34 +0530

    commit

---
+++ src/StudentPackage/BaseController.java
@@ -0,0 +1,43 @@
+package StudentPackage;
+
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+import java.io.IOException;
+
+public abstract class BaseController
+    {
+        public void handleLogout(){
+            Session.clearSession();
+
+            FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("FXMLfiles/loginform.fxml"));
+            try {
+                Parent root = loader.load();
+                Scene scene = new Scene(root);
+                TechmisApp.getPrimaryStage().setScene(scene);
+
+            } catch (IOException e) {
+                System.out.println("error: " + e.getMessage());
+            }
+        }
+
+        public void handleEditprofile(){
+            FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("studentRole/view/EditProfile.fxml"));
+            try {
+                Parent root = loader.load();
+                Scene scene = new Scene(root);
+                TechmisApp.getPrimaryStage().setScene(scene);
+
+            } catch (IOException e) {
+                System.out.println("error: " + e.getMessage());
+            }
+        }
+
+        public abstract  void loadProfileData();
+
+
+    }
+
+
