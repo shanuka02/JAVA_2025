dc7ace6e5c2c83087a2652ba2d4e6e7ae4701d4d
Poojani-janitha <jayarathnahpj23@gmail.com>
2025-04-27 22:49:15 +0530

    finaaaaaaal

--- src/Admin/ApplicationDrive.java
+++ src/Admin/ApplicationDrive.java
@@ -1,5 +1,6 @@
 package Admin;
 
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
--- src/Lecture/ManageCourseDetailsController.java
+++ src/Lecture/ManageCourseDetailsController.java
@@ -1,5 +1,6 @@
 package Lecture;
 
+
 import Admin.ApplicationDrive;
 import javafx.fxml.FXML;
 
@@ -8,6 +9,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 import javafx.event.ActionEvent;
 import javafx.stage.Stage;
